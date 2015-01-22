29640b4abef3d7aac40dbb2e6620a4b0038e4d67
Roman Birg <roman@cyngn.com>
2015-01-21 18:59:20 -0800

    Add NFC QS Tile
    More cleanup on QS tiles
    * Make strings & states more consistent to AOSP tiles
    * Remove unused/duplicate strings
    * Fix copyright headers
    Change-Id: Ib97f216c31c96e23d72ce7bf7e63b16b65f1bee0
    
    SystemUI: update quick tile animations and icons
    Use new AnimationIcons and ResourceIcon classes added with 5.1
    Change-Id: Ib904ac0cda2a2787f960893a3434cf21f392af3b
    
    Fix tile disapearing randomly
    
    PureNexus edits:
    *properly forward port to 7.0
    *add is available boolean hasSystemFeature(PackageManager.FEATURE_NFC)

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -57,6 +57,7 @@ import com.android.systemui.qs.tiles.ImeTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.MusicTile;
+import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
@@ -449,6 +450,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("caffeine")) return new CaffeineTile(this);
         else if (tileSpec.equals("ime")) return new ImeTile(this);
         else if (tileSpec.equals("music")) return new MusicTile(this);
+        else if (tileSpec.equals("nfc")) return new NfcTile(this);
         else if (tileSpec.equals("reboot")) return new RebootTile(this);
         else if (tileSpec.equals("screenshot")) return new ScreenshotTile(this);
         else if (tileSpec.equals("sound")) return new SoundTile(this);
