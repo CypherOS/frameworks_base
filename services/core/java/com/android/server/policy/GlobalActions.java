e1e95f716bdb47d62e917b6a59fb7e546648fdce
Dave Kessler <activethrasher00@gmail.com>
2015-02-18 10:27:58 +0200

    GlobalActions: Always add power off option to power menu
    Change-Id: I3083cda3b8c665a65f1260984a5be41faa384510

--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -279,6 +279,9 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             actionsArray = mActions.split("\\|");
         }
 
+        // Always add the power off option
+        mItems.add(new PowerAction());
+
         ArraySet<String> addedKeys = new ArraySet<String>();
         for (int i = 0; i < actionsArray.length; i++) {
             String actionKey = actionsArray[i];
@@ -287,7 +290,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 continue;
             }
             if (GLOBAL_ACTION_KEY_POWER.equals(actionKey)) {
-                mItems.add(new PowerAction());
+                continue;
             } else if (GLOBAL_ACTION_KEY_RESTART.equals(actionKey)) {
                 mItems.add(new RebootAction());
             } else if (GLOBAL_ACTION_KEY_SCREENSHOT.equals(actionKey)) {
