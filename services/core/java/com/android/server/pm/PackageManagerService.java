015ddd84b4bc8bdb45db63e77955a508c48ce275
Roman Birg <roman@cyngn.com>
2015-06-10 10:34:35 -0700

    Allow permissions to be granted via whitelisted signatures
    When an application defines a permission, it can now add a new
    attribute "allowViaWhitelist", a boolean value. If set to true,
    the permission may be granted to a package signed with a predefined key,
    if it is defined via <allow-permission> in
    /system/etc/permissions/someapp.xml.
    
    Since this is a hidden attribute, it must use the prv namespace XML
    declaration. E.g.: add the following to the <manifest> tag:
    
           xmlns:androidprv="http://schemas.android.com/apk/prv/res/android"
    
    In the permission declaration:
    
            <permission
                android:name=""
                android:protectionLevel="signature"
                androidprv:allowViaWhitelist"true" />
    
    And a corresponding entry in /system/etc/permissions/someapp.xml:
    
            <allow-permission
                name="some.android.PERMISSION"
                signature="<known public signature>" />
    
    Note: if the permission never declares "allowViaWhitelist", then the
    whitelisted permissions will be ignored.
    
    Change-Id: Ie4597a07eb0a193375fa2724bd9cf468184a7926
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    PackageManager: copy allowViaWhitelist flag for perm trees
    
    Ref: CYNGNOS-622
    Change-Id: Ic52f179d79ac69a055f994df05a502f893435a26
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Explicitly declare which permissions can be granted via signature
    
    This moves the older <allow-permissions> implementation inline with the
    newer one, which requires any permission that can be granted via a
    signature to explicitly add the attribute allowViaWhitelist="true".
    
    Change-Id: Ie9f9bf6e2138075b750c58090dcd7030eff174f1
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Allow CAPTURE_AUDIO_HOTWORD to be whitelisted by signature.
    
    Change-Id: Ia4589c0fd5480a6f462812f5081160262632f09f
    
    Allow additional permissions to be whitelisted by signature.
    
    - android.permission.SET_PREFERRED_APPLICATIONS.
    - android.permission.INTERACT_ACROSS_USERS_FULL.
    
    Change-Id: I3fdb3fd412d3eb641a5745eb298afa1121540ccf
    
    allow GET_ACCOUNTS_PRIVELEGED to be whitelisted
    
    Change-Id: Idc5e5535476015800e47f0a3a472729a926dd7b1
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    allow priveleged phone state permission to be whitelisted
    
    Ref: CYNGNOS-1660
    
    Change-Id: I377ad833e44d5c54bbcf2f7447fe5383edc457fb
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    allow whitelisting of ACCESS_KEYGUARD_SECURE_STORAGE
    
    Ref: CYNGNOS-1727
    
    Change-Id: I7151ce3fa7c57ee89fecf691d6e92c4ab39ecf37
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Allow granting permissions based on signature in <allow-permission/>
    
    This patch allows us to either specify a sharedUserId or a package
    signature to use when granting the specific permission.
    
    Change-Id: I8aed78d40316e0e94ac1bfefc7c4a3016a2a9a6b
    
    Explicitly declare which permissions can be granted via signature
    
    This moves the older <allow-permissions> implementation inline with the
    newer one, which requires any permission that can be granted via a
    signature to explicitly add the attribute allowViaWhitelist="true".
    
    Change-Id: Ie9f9bf6e2138075b750c58090dcd7030eff174f1
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -298,7 +298,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -654,6 +653,7 @@ public class PackageManagerService extends IPackageManager.Stub {
     final int[] mGlobalGids;
     final SparseArray<ArraySet<String>> mSystemPermissions;
     final ArrayMap<String, FeatureInfo> mAvailableFeatures;
+    final ArrayMap<Signature, ArraySet<String>> mSignatureAllowances;
 
     // If mac_permissions.xml was found for seinfo labeling.
     boolean mFoundPolicyFile;
@@ -2157,6 +2157,7 @@ public class PackageManagerService extends IPackageManager.Stub {
         mGlobalGids = systemConfig.getGlobalGids();
         mSystemPermissions = systemConfig.getSystemPermissions();
         mAvailableFeatures = systemConfig.getAvailableFeatures();
+        mSignatureAllowances = systemConfig.getSignatureAllowances();
 
         mProtectedPackages = new ProtectedPackages(mContext);
 
@@ -4027,6 +4028,7 @@ public class PackageManagerService extends IPackageManager.Stub {
         if (!compareStrings(pi1.nonLocalizedLabel, pi2.nonLocalizedLabel)) return false;
         // We'll take care of setting this one.
         if (!compareStrings(pi1.packageName, pi2.packageName)) return false;
+        if (pi1.allowViaWhitelist != pi2.allowViaWhitelist) return false;
         // These are not currently stored in settings.
         //if (!compareStrings(pi1.group, pi2.group)) return false;
         //if (!compareStrings(pi1.nonLocalizedDescription, pi2.nonLocalizedDescription)) return false;
@@ -4149,6 +4151,16 @@ public class PackageManagerService extends IPackageManager.Stub {
             throw new SecurityException("Permission " + bp.name
                     + " is not a changeable permission type");
         }
+    }
+
+    private boolean isAllowedSignature(PackageParser.Package pkg, String permissionName) {
+        for (Signature pkgSig : pkg.mSignatures) {
+            ArraySet<String> perms = mSignatureAllowances.get(pkgSig);
+            if (perms != null && perms.contains(permissionName)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
@@ -7634,7 +7646,7 @@ public class PackageManagerService extends IPackageManager.Stub {
     Collection<PackageParser.Package> findSharedNonSystemLibraries(PackageParser.Package p) {
         if (p.usesLibraries != null || p.usesOptionalLibraries != null) {
             ArrayList<PackageParser.Package> retValue = new ArrayList<>();
-            Set<String> collectedNames = new HashSet<>();
+            Set<String> collectedNames = new ArraySet<>();
             findSharedNonSystemLibrariesRecursive(p, retValue, collectedNames);
 
             retValue.remove(p);
@@ -9045,6 +9057,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                             bp.perm = p;
                             bp.uid = pkg.applicationInfo.uid;
                             bp.sourcePackage = p.info.packageName;
+                            bp.allowViaWhitelist = p.info.allowViaWhitelist;
                             p.info.flags |= PermissionInfo.FLAG_INSTALLED;
                         } else if (!currentOwnerIsSystem) {
                             String msg = "New decl " + p.owner + " of permission  "
@@ -9058,6 +9071,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                 if (bp == null) {
                     bp = new BasePermission(p.info.name, p.info.packageName,
                             BasePermission.TYPE_NORMAL);
+                    bp.allowViaWhitelist = p.info.allowViaWhitelist;
                     permissionMap.put(p.info.name, bp);
                 }
 
@@ -9071,6 +9085,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                             bp.perm = p;
                             bp.uid = pkg.applicationInfo.uid;
                             bp.sourcePackage = p.info.packageName;
+                            bp.allowViaWhitelist = p.info.allowViaWhitelist;
                             p.info.flags |= PermissionInfo.FLAG_INSTALLED;
                             if ((policyFlags&PackageParser.PARSE_CHATTY) != 0) {
                                 if (r == null) {
@@ -10469,7 +10484,6 @@ public class PackageManagerService extends IPackageManager.Stub {
                 & PermissionInfo.PROTECTION_FLAG_PRIVILEGED) != 0) {
             if (isSystemApp(pkg)) {
                 // For updated system applications, a system permission
-                // is granted only if it had been defined by the original application.
                 if (pkg.isUpdatedSystemApp()) {
                     final PackageSetting sysPs = mSettings
                             .getDisabledSystemPkgLPr(pkg.packageName);
@@ -10564,6 +10578,9 @@ public class PackageManagerService extends IPackageManager.Stub {
                 // this app is a setup wizard, then it gets the permission.
                 allowed = true;
             }
+        }
+        if (!allowed && bp.allowViaWhitelist) {
+            allowed = isAllowedSignature(pkg, perm);
         }
         return allowed;
     }
