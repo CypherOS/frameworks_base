4fe728e10e0e74b4df466cbea32c5c66aa6ba37a
maxwen <max.weninger@gmail.com>
2015-11-21 18:18:06 -0500

    [1/2] base: allow to swap volume buttons rotation based Forward port from 4.4 but became more complicated now Also check the volume change sound disablement earlier
    Squashed:
    omnirom/android_frameworks_base@7f75f2b
    fix fc on volume key press in 3 dotmenus (Me, george, camcory)
    
    Change-Id: Ie721bb63ed8a1b713855bb69137dec4e04b33d48

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3688,6 +3688,12 @@ public final class Settings {
           * @hide
           */
          public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+		 
+	    /**
+         * Change volume up and down handlign based on rotation
+         * @hide
+         */
+        public static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
