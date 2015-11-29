74c41452526772685606ec465a3aeaabff265886
kufikugel <kufikugel@googlemail.com>
2015-11-29 05:51:19 +0000

    Add Music QS Tile
    picked from here: http://goo.gl/ehaoMU
    
    Adapted to lollipop from commit:
    Author: kufikugel
    Quicksettings customizations Slim style
    d85304fac9bde550beeeeca0dba2cd49686f20d7
    
    Adapted double click to open music app from commit:
    Author: Jubakuba
    SystemUI: Pimp-up Media Tile (1/1)
    5a14de5db237969607e0aabb9b8c04737b122dd3
    
    AICPfy:
    PS1: Gave proper credit to kufikugel for the original code
    PS2: Fix build by overriding all needed methids
    
    DU edits (I'm sure the benzo fuck head will delete)
    - Adapted to M and made to work with AOSP Quick settings
    
    PureNexus Edits: Vectorz!!

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -52,6 +52,7 @@ import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
+import com.android.systemui.qs.tiles.MusicTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UserTile;
@@ -436,6 +437,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("saver")) return new DataSaverTile(this);
         else if (tileSpec.equals("ambient_display")) return new AmbientDisplayTile(this);
         else if (tileSpec.equals("sync")) return new SyncTile(this);
+        else if (tileSpec.equals("music")) return new MusicTile(this);
         else if (tileSpec.equals(NightModeTile.NIGHT_MODE_SPEC))
             return new NightModeTile(this);
         // Intent tiles.
