a9b45fbcb077496833066aba131aed493eef7014
rascarlo <rascarlo@gmail.com>
2015-12-01 19:18:53 +0100

    policy: Allow volume buttons to wake device
    Change-Id: Ia32e77b4737efd5816da1b4cfb74138fc357a46b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4370,6 +4370,12 @@ public final class Settings {
         public static final String PROXIMITY_ON_WAKE = "proximity_on_wake";
 
         /**
+         * Volume rocker wake
+         * @hide
+         */
+        public static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
