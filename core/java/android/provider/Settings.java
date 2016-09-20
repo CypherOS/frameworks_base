c37ec42436aeba2868869e90c089b7f41ba13713
ezio84 <brabus84@gmail.com>
2016-09-20 14:07:03 +0200

    Data tile: make it customizable by the user [1/2]
    * if advanced tile is enabled: one tap to switch on/off data, long press to show details view
    * if advanced tile is disabled: stock behavior, one tap for details view and long press for Cellular settings
    
    Change-Id: Icbc323b78c28d5ba84eb5f99b8210f075fd62e26
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7018,6 +7018,12 @@ public final class Settings {
          * @hide
          */
         public static final String QS_TILES = "sysui_qs_tiles";
+		
+		/**
+         * Whether to use one tap tile action to enable or disable data 
+         * @hide
+         */
+        public static final String QS_DATA_ADVANCED = "qs_data_advanced";
 
         /**
          * Whether preloaded APKs have been installed for the user.
