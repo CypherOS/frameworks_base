19c5701c44eea298c8a601fe78a070d1114a3721
Chris <chriscrawf95@gmail.com>
2016-10-19 16:17:12 -0700

    FWB: Add TunerService import for weathercontroller

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -58,6 +58,7 @@ import com.android.systemui.statusbar.policy.NextAlarmController;
 import com.android.systemui.statusbar.policy.UserInfoController;
 import com.android.systemui.statusbar.policy.WeatherController;
 import com.android.systemui.statusbar.policy.WeatherControllerImpl;
+import com.android.systemui.tuner.TunerService;
 
 import java.text.NumberFormat;
 
