13c196758a2d9a784779cdea157c33e6805a570c
Chris <chriscrawf95@gmail.com>
2016-10-23 14:42:20 -0700

    Revert'SystemUI : Port forward statusbar brightness control'
    This is CM's modded brightness control, we want AOSP. The preference
    field is missing, which explains why it wasn't working

--- packages/SystemUI/src/com/android/systemui/settings/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/BrightnessController.java
@@ -44,7 +44,7 @@ public class BrightnessController implements ToggleSlider.Listener {
      * {@link android.provider.Settings.System#SCREEN_AUTO_BRIGHTNESS_ADJ} uses the range [-1, 1].
      * Using this factor, it is converted to [0, BRIGHTNESS_ADJ_RESOLUTION] for the SeekBar.
      */
-    public static final float BRIGHTNESS_ADJ_RESOLUTION = 2048;
+    private static final float BRIGHTNESS_ADJ_RESOLUTION = 2048;
 
     private final int mMinimumBacklight;
     private final int mMaximumBacklight;
