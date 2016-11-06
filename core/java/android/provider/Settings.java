7d24621c0497d1961dc71b39f236c7df0f63c509
Alex Cruz <mazdarider23@gmail.com>
2016-11-06 20:37:36 +0100

    Optional screenshot type [1/2]
    Change-Id: Iaa579c1f53e504f7dfc3189e1dfb8e399e26da0b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4136,6 +4136,12 @@ public final class Settings {
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
         /**
+         * Whether to allow fullscreen screenshots or let the user select which area to screenshot
+         * @hide
+         */
+        public static final String SCREENSHOT_TYPE = "screenshot_type";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
