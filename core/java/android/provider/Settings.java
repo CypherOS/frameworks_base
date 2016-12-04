b45dc02eb8d21edbfc27414dd95663df4fc9ded3
Chris <chriscrawf95@gmail.com>
2016-12-03 23:12:42 -0500

    base:[SQUASH] Implement battery styles
    This commit squashes the following commits below:
    
    SystemUI: Implement battery level text customisations
        3 options - hidden, show beside, or show inside.
    
        Forward port the battery text-related changes from CM 13.0:
        2e0a11bb0bbf22c32f6e15d97ccac10414ec7b44 Forward port battery icon options
        738f5a87a361c9c1027e3764072a37a50467fe6d SystemUI: Expose values in status bar expanded header
        9500061a111fed25bed3ad30173d8e35063f32d0 Themes: Expose hard code for themes in SystemUI
    Author: @zwliew
    
    SystemUI: Implement hidden and text battery styles
        Forward port the hidden and text battery styles from CM 13.0:
        2e0a11bb0bbf22c32f6e15d97ccac10414ec7b44 Forward port battery icon options
    
        Change-Id: I30d95edf9c6fe6afb343aff5effe132f94c49eeb
    Author: @zwliew
    
    SystemUI: Add VectorDrawable battery styles
        The current implementation of BatteryMeterView uses arrays of points
        that are drawn programmatically.  This limits the ability to overlay
        or theme the battery. By utilizing VectorDrawables and
        AnimatedVectorDrawables we can achieve the same look and feel of
        stock while allowing for more unique battery meters.
    
        Implement portrait, landscape and circle AVD battery styles.
        The various dimensions adjustments are to accommodate the
        slightly different sizes of the AVD battery styles.
    
        Forward port the AVD battery commits from CM 13.0:
        db5cf5dc36c629b9afba8f883ef5e33a33608409 SysUI: Use VectorDrawable's for rendering battery
        d37a5aa57bb1c5d958abe036b5558ba4dac270c5 SysUI: Fix coloring of frame and bolt
        2b21b18e109ddab30d7a0ff47dd900e55167ec37 SysUI: Use linear interpolator for battery level
        5953be013f1f3fe14b5f4176b743c4032e114bfc SysUI: Allow styling of battery text/charge blend mode
        e3ddac3a775d2e8808da40d302794e9b50016d9b SysUI: Adjust battery dimensions for better look
        1530ec9e734cae8bfd966f3c43b3be9041386c29 SystemUI: Set circle battery path as untranslatable
    Author: @0xD34D
    
    Fix battery percentage not showing on keyguard when set to text only
    Author: @tsubus
    
    Port PA battery circle style
        This commit adds the infamous PA battery circle style from Marshmallow.
        The original implementation replaces CM's circle battery style. But, why
        not make it an option alongside? The code has been broken up and refractored
        to be a standalone battery style to be chosen in the Styles menu from Settings.
    
        Also a patch is included to disable Bolt checks, cherry-picked from the
        original styles commit here: AOSPA@android_frameworks_base/c5ef709
    Author: @Grarak
    
    Fix battery text scaling issue
        This should of scaled correctly out the box but Google had a derp and placed it in a class
        that is just dead code
    
        https://github.com/DirtyUnicorns/android_frameworks_base/blob/n/packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java#L216
    
        This class is removed by Google in 7.1 so for now we're just keeping it around because is not
        really hurting anything either.
    
        android/platform_frameworks_base@5c37344
    
        Change-Id: I06ca9318cd1eb2215589cd68839795754a9113ac
    Author: @camcory
    
    Sync battery tile icon with statusbar
        Change-Id: I92914dbf2334e71a0f9998578cd0079edb546a81
    Author: @tsubus
    
    CypherOS Edits:
      Remove Caf charging animation and battery percentage implementation
    
    Change-Id: I4a8764a41d01f80d6970656826c87dd076e0a6f8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6798,6 +6798,28 @@ public final class Settings {
          */
         public static final String DEMO_USER_SETUP_COMPLETE
                 = "demo_user_setup_complete";
+				
+		/**
+         * Status bar battery %
+         * 0: Hide the battery percentage
+         * 1: Display the battery percentage inside the icon
+         * 2: Display the battery percentage next to the icon
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+		
+		/**
+         * Display style of the status bar battery information
+         * 0: Display the battery an icon in portrait mode
+         * 2: Display the battery as a solid circle
+         * 4: Display the battery as a circle
+         * 5: Hide the battery status information
+         * 6: Display the battery an icon in landscape mode
+         * 7: Display the battery as plain text
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
 
         /**
          * Specifies whether the web action API is enabled.
@@ -6919,7 +6941,9 @@ public final class Settings {
             DOZE_ENABLED,
             DOZE_PULSE_ON_PICK_UP,
             DOZE_PULSE_ON_DOUBLE_TAP,
-            NFC_PAYMENT_DEFAULT_COMPONENT
+            NFC_PAYMENT_DEFAULT_COMPONENT,
+			STATUS_BAR_SHOW_BATTERY_PERCENT,
+			STATUS_BAR_BATTERY_STYLE
         };
 
         /**
