a89366cb14685a3bc569ba7538023fc3c5e0dc55
Anas Karbila <anaskarbila@aospa.co>
2016-12-11 00:55:02 +0100

    SystemUI: add nfc tile with android beam mode
    * Drawable taken from material.io
    
    CypherOS Edits: This is based on Anas's Patchset 3, which
    includes a detail view and support for switching to Android Beam.
    Unfortunately this was removed to follow-up submission to
    AOSP, but i like this version better.
    
    Signed-off-by: Chris <chris@cypheros.co>
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>
    Change-Id: I7dfaf3a4dca0c01d24be397e43ab131eb9885f62

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -55,6 +55,7 @@ import com.android.systemui.qs.tiles.ImmersiveTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
+import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.UserTile;
@@ -439,6 +440,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("immersive")) return new ImmersiveTile(this);
         else if (tileSpec.equals("alertslider")) return new AlertSliderTile(this);
         else if (tileSpec.equals("screenshot")) return new ScreenshotTile(this);
+        else if (tileSpec.equals("nfc")) return new NfcTile(this);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
