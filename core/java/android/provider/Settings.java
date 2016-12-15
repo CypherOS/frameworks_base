42c093fa2cd6e3142da83e363defe58b03dbfccc
Chris <chriscrawf95@gmail.com>
2016-12-15 13:35:03 -0500

    Fwb: Properly hide field from apichecks
    Change-Id: Ia10a8393e03af917a82794296c13a9c5a68a47db

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3997,7 +3997,7 @@ public final class Settings {
          */
         public static final String STATUS_BAR_SHOW_CARRIER = "status_bar_show_carrier";
 		
-		/*
+		/**
          * Dyanamic Navbar Switch
          * @hide
          */
