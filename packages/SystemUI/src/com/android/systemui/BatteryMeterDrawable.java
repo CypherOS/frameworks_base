c3843c14dccee34a4987c1a1fc91eacc23293493
Chris <chriscrawf95@gmail.com>
2017-01-22 22:58:18 -0500

    BatteryMeterDrawable: Display warning symbol inline
    Warning symbol a.k.a "!" overlaps the battery style. We
    don't need a preset paremeter. Let's ensure it operates
    the same as the text size
    
    Change-Id: Ic2acaad3b0407ab03f3a4e4a2637b5fcfd257054

--- packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -324,7 +324,6 @@ public class BatteryMeterDrawable extends Drawable implements
         super.setBounds(left, top, right, bottom);
         mHeight = bottom - top;
         mWidth = right - left;
-        mWarningTextPaint.setTextSize(mHeight * 0.75f);
         mWarningTextHeight = -mWarningTextPaint.getFontMetrics().ascent;
     }
 
