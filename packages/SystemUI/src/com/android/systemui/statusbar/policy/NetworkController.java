4f8af1686ab3fe807ca0669c11c3eab843410a31
Chris <chriscrawf95@gmail.com>
2017-02-02 19:52:23 -0500

    base: CAF finalizations
    DO NOT MERGE!
    
    Updates for CAF related handsets. This is a partial merge from previous
    CypherOS releases, pulled from the N7.1 branch.
    
    Change-Id: I841bcb1ac9738e8c03219386a32f20ec0437b0ed

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
@@ -46,10 +46,8 @@ public interface NetworkController {
                 boolean activityIn, boolean activityOut, String description) {}
 
         default void setMobileDataIndicators(IconState statusIcon, IconState qsIcon, int statusType,
-                int qsType, boolean activityIn, boolean activityOut, int dataActivityId,
-                int mobileActivityId, int stackedDataIcon, int stackedVoiceIcon,
-                String typeContentDescription, String description,
-                boolean isWide, int subId) {}
+                int qsType, boolean activityIn, boolean activityOut, String typeContentDescription,
+                String description, boolean isWide, int subId) {}
         default void setSubs(List<SubscriptionInfo> subs) {}
         default void setNoSims(boolean show) {}
 
@@ -58,15 +56,6 @@ public interface NetworkController {
         default void setIsAirplaneMode(IconState icon) {}
 
         default void setMobileDataEnabled(boolean enabled) {}
-    }
-
-    public interface SignalCallbackExtended extends SignalCallback {
-        void setMobileDataIndicators(IconState statusIcon, IconState qsIcon, int statusType,
-                int qsType, boolean activityIn, boolean activityOut, int dataActivityId,
-                int mobileActivityId, int stackedDataIcon, int stackedVoiceIcon,
-                String typeContentDescription, String description, boolean isWide,
-                int subId, int dataNetworkTypeInRoamingId, int embmsIconId,
-                int imsIconId, boolean isImsOverWifi);
     }
 
     public interface EmergencyListener {
