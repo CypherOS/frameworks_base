f1176ec474eaf5e60e8b0a44e5f59bef3d85f1cc
Chris <chriscrawf95@gmail.com>
2017-02-10 13:31:11 -0500

    base: User configurable rotation settings [2/2]
    Brings in required code for keyguard rotation to be
    optional for the user. User's can opt-in to keyguard
    rotation, only allow system rotation, or disable rotation
    completely.
    
    UserContentObserver has been brought back from here:
    CypherOS@frameworks_base/760e8c9ff683e5c53fc7d7d3482fdf60cdddfcf7
    since it serves it's purpose for custom system settings.
    
    Change-Id: I2ac73e218a408d8dbb6bc42fcab63018bb0e10cf

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3599,6 +3599,13 @@ public final class Settings {
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
          * the setting value. See an example above.
          */
+		
+		/**
+         * Control whether the accelerometer will be used to change lockscreen
+         * orientation.  If 0, it will not be used; if 1, it will be used by default.
+         * @hide
+         */
+        public static final String LOCKSCREEN_ROTATION = "lockscreen_rotation";
 
         /**
          * Whether user can enable/disable navigation bar.
