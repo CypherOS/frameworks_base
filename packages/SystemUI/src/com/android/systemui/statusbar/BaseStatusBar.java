99701f5e6e3722dfe64605679f2325d429ec6b7f
Chris <chris@cypheros.co>
2017-03-08 00:39:46 -0500

    SystemUI: Heads Up customizations [2/2]
    A squashed commit of headsup features and improvements
    
    SystemUI: Add heads up tile
    
      PureNexus Edits:
      adapt for nougat
    
    Author: Michael Bestas <mikeioannina@gmail.com>
    Date: Wed Apr 22 02:15:28 2015 +0300
    6 files changed, 171 insertions(+), 1 deletion(-)
    
    Improve Heads up toggle [1/2]
    
      - Add multiuser support, provide a system setting to override heads up
        showing. User can disable heads up within this one so we don't need to
        mess with Settings.Global heads up dependencies and break Google wide
        implementations like priority filtering etc.
    
      - updated heads up quicksetting tile to reflect changes as well
    
    PureNexus Edits:
    *Adapted for Marshmallow/Nougat
    
    Author: BigBrother1984 <carlosavignano@aospa.co>
    Date: Sat Mar 26 16:55:22 2016 -0400
    4 files changed, 41 insertions(+), 22 deletions(-)
    
    HeadsUp: add timeout option (1/2)
    
      Forward ported to marshmallow/nougat
      By: @BeansTown106
    this was basically a
      rewrite as everything about it
    
    Author: Lars Greiss <kufikugel@googlemail.com>
    Date: Sat Mar 26 21:18:14 2016 -0400
    2 files changed, 16 insertions(+), 2 deletions(-)
    Change-Id: Ib2203f53de06a0d594a1c83c55bfeeb4844151c2
    
    Base: HeadsUp snooze function [1/2]
    
      5.1 introduced a snooze feature which
      activates when swiping up.
    Default snooze
      time is 1 minute. Let's make it configurable!
    
    Forward ported to marshmallow/Nougat By:@BeansTown106
    
      - Pretty much a rewrite using the logic from my heads up timeout port
    
      - Also added a second toast for 1 minute instead of saying 1 minutes
    
    Author: Lokesh Chamane <lokesh.c703@gmail.com>
    Date: Sat Apr 9 19:54:40 2016 -0400
    3 files changed, 31 insertions(+), 21 deletions(-)
    
    Make Heads Up snooze toast more informative
    
      - Tell the user which app is snoozing
      - Also center the toast to account for big app names
      Example: SystemUI - Peeking notifications are snoozing for 1 minute.
    
    Author: beanstown106 <nbenis106@gmail.com>
    Date: Tue Nov 1 21:37:39 2016 -0400
    2 files changed, 25 insertions(+), 8 deletions(-)
    
    Signed-off-by: Chris <chris@cypheros.co>
    Change-Id: Ie030ea9d30f9de81dbe09d8a64666f89982f2d2e

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -213,6 +213,7 @@ public abstract class BaseStatusBar extends SystemUI implements
     private float mFontScale;
 
     protected boolean mUseHeadsUp = false;
+    protected boolean mHeadsUpUserEnabled = false;
     protected boolean mHeadsUpTicker = false;
     protected boolean mDisableNotificationAlerts = false;
 
@@ -2550,7 +2551,7 @@ public abstract class BaseStatusBar extends SystemUI implements
     }
 
     protected boolean shouldPeek(Entry entry, StatusBarNotification sbn) {
-        if (!mUseHeadsUp || isDeviceInVrMode()) {
+        if (!mUseHeadsUp || !mHeadsUpUserEnabled || isDeviceInVrMode()) {
             return false;
         }
 
