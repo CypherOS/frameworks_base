d71f6bc738530c60efe0180ae93b3cee288a33fc
Anas Karbila <anaskarbila@gmail.com>
2017-03-31 22:24:20 +0900

    SystemUI: Fix priority only zen footer icon size
    * This should be 24 to align with the other zen footer icons
    
    Change-Id: I4def203124d9537ed2faec359816eeb1b63d9b27
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- packages/SystemUI/src/com/android/systemui/volume/ZenFooter.java
+++ packages/SystemUI/src/com/android/systemui/volume/ZenFooter.java
@@ -143,7 +143,7 @@ public class ZenFooter extends LinearLayout {
     }
 
     public void update() {
-        mIcon.setImageResource(isZenPriority() ? R.drawable.ic_dnd_priority /* TODO> Add properly sized drawable*/
+        mIcon.setImageResource(isZenPriority() ? R.drawable.ic_dnd_priority
                         : isZenAlarms() ? R.drawable.ic_dnd
                         : isZenNone() ? R.drawable.ic_dnd_total_silence
                         : 0);
