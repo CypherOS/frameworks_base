c528cf5de16cb448f1317654fea9302b7f48eeae
Chris <chris@cypheros.co>
2017-04-09 12:11:27 -0400

    Compilation fixes after merge
    - Remove multithread decode for now
    
    Change-Id: I3250b030bc7ac5b3f8046adac76b96b6a5268b97

--- cmds/bootanimation/BootAnimation.h
+++ cmds/bootanimation/BootAnimation.h
@@ -26,8 +26,6 @@
 #include <EGL/egl.h>
 #include <GLES/gl.h>
 
-#include <utils/Thread.h>
-
 class SkBitmap;
 
 namespace android {
@@ -35,17 +33,11 @@ namespace android {
 class Surface;
 class SurfaceComposerClient;
 class SurfaceControl;
-#ifdef MULTITHREAD_DECODE
-class FrameManager;
-#endif
 
 // ---------------------------------------------------------------------------
 
 class BootAnimation : public Thread, public IBinder::DeathRecipient
 {
-#ifdef MULTITHREAD_DECODE
-    friend class FrameManager;
-#endif
 public:
                 BootAnimation();
     virtual     ~BootAnimation();
@@ -154,8 +146,6 @@ private:
 
     void checkExit();
 
-    static SkBitmap *decode(const Animation::Frame& frame);
-
     sp<SurfaceComposerClient>       mSession;
     AssetManager mAssets;
     Texture     mAndroid[2];
@@ -175,49 +165,6 @@ private:
     SortedVector<String8> mLoadedFiles;
     sp<TimeCheckThread> mTimeCheckThread;
 };
-
-#ifdef MULTITHREAD_DECODE
-class FrameManager {
-public:
-    struct DecodeWork {
-        const BootAnimation::Animation::Frame *frame;
-        SkBitmap *bitmap;
-        size_t idx;
-    };
-
-    FrameManager(int numThreads, size_t maxSize,
-            const SortedVector<BootAnimation::Animation::Frame>& frames);
-    virtual ~FrameManager();
-
-    SkBitmap* next();
-
-protected:
-    DecodeWork getWork();
-    void completeWork(DecodeWork work);
-
-private:
-
-    class DecodeThread : public Thread {
-    public:
-        DecodeThread(FrameManager* manager);
-        virtual ~DecodeThread() {}
-    private:
-        virtual bool threadLoop();
-        FrameManager *mManager;
-    };
-
-    size_t mMaxSize;
-    size_t mFrameCounter;
-    size_t mNextIdx;
-    const SortedVector<BootAnimation::Animation::Frame>& mFrames;
-    Vector<DecodeWork> mDecodedFrames;
-    pthread_mutex_t mBitmapsMutex;
-    pthread_cond_t mSpaceAvailableCondition;
-    pthread_cond_t mBitmapReadyCondition;
-    bool mExit;
-    Vector<sp<DecodeThread> > mThreads;
-};
-#endif
 
 // ---------------------------------------------------------------------------
 
