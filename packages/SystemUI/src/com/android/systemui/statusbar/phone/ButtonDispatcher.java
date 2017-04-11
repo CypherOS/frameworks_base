502e20e66b94fe69c7dffc798edec6fe665784e6
Chris <chris@cypheros.co>
2017-04-10 21:22:23 -0400

    ButtonDispatcher: Fallback to setLandscape
    Change-Id: I2db4a0b58aa5460518d7fde0650c8bcdd0ea8e09

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ButtonDispatcher.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ButtonDispatcher.java
@@ -40,7 +40,6 @@ public class ButtonDispatcher {
     private int mImageResource = -1;
     private Drawable mImageDrawable;
     private View mCurrentView;
-    private boolean mVertical;
 
     public ButtonDispatcher(int id) {
         mId = id;
@@ -48,6 +47,13 @@ public class ButtonDispatcher {
 
     void clear() {
         mViews.clear();
+    }
+	
+	void addView(View view, boolean landscape) {
+        addView(view);
+        if (view instanceof ButtonInterface) {
+            ((ButtonInterface) view).setLandscape(landscape);
+        }
     }
 
     void addView(View view) {
@@ -68,10 +74,6 @@ public class ButtonDispatcher {
             ((ButtonInterface) view).setImageResource(mImageResource);
         } else if (mImageDrawable != null) {
             ((ButtonInterface) view).setImageDrawable(mImageDrawable);
-        }
-
-        if (view instanceof  ButtonInterface) {
-            ((ButtonInterface) view).setVertical(mVertical);
         }
     }
 
@@ -184,17 +186,6 @@ public class ButtonDispatcher {
         }
     }
 
-    public void setVertical(boolean vertical) {
-        mVertical = vertical;
-        final int N = mViews.size();
-        for (int i = 0; i < N; i++) {
-            final View view = mViews.get(i);
-            if (view instanceof ButtonInterface) {
-                ((ButtonInterface) view).setVertical(vertical);
-            }
-        }
-    }
-
     /**
      * Interface for button actions.
      */
@@ -205,7 +196,7 @@ public class ButtonDispatcher {
 
         void abortCurrentGesture();
 
-        void setVertical(boolean vertical);
+        void setLandscape(boolean landscape);
 
         void setCarMode(boolean carMode);
     }
--- packages/SystemUI/src/com/google/android/systemui/OpaLayout.java
+++ packages/SystemUI/src/com/google/android/systemui/OpaLayout.java
@@ -584,10 +584,6 @@ public class OpaLayout extends FrameLayout implements ButtonDispatcher.ButtonInt
     public void setImageResource(int resId) {
         ((ImageView) mWhite).setImageResource(resId);
     }
-	
-	public void setVertical(boolean vertical) {
-		setLandscape(!vertical);
-	}
 
     public void setLandscape(boolean landscape) {
         mIsLandscape = landscape;
