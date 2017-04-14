e43d666eff6c996e62c1bf349c62b944026aafa0
Chris <chris@cypheros.co>
2017-04-14 00:41:07 -0400

    OpaLayout: Correct animation orientation
    7.1.2 made changes to the navbar orientation, which broke the pixel
    animation, forcing it to animate vertically. Let's implement the new
    method and reverse it's order
    
    Change-Id: I663a6843e72bd0abd58100355c8b3bb7779ff9bf

--- packages/SystemUI/src/com/google/android/systemui/OpaLayout.java
+++ packages/SystemUI/src/com/google/android/systemui/OpaLayout.java
@@ -59,7 +59,7 @@ public class OpaLayout extends FrameLayout implements ButtonDispatcher.ButtonInt
     private int mAnimationState;
     private final ArraySet<Animator> mCurrentAnimators;
 
-    private boolean mIsLandscape;
+    private boolean mVertical;
     private boolean mIsPressed;
     private boolean mLongClicked;
     private boolean mOpaEnabled;
@@ -293,7 +293,7 @@ public class OpaLayout extends FrameLayout implements ButtonDispatcher.ButtonInt
     private ArraySet<Animator> getCollapseAnimatorSet() {
         final ArraySet<Animator> set = new ArraySet<Animator>();
         Animator animator;
-        if (mIsLandscape) {
+        if (!mVertical) {
             animator = getDeltaAnimatorY(mRed, mCollapseInterpolator, -getPxVal(R.dimen.opa_line_x_collapse_ry), COLLAPSE_ANIMATION_DURATION_RY);
         } else {
             animator = getDeltaAnimatorX(mRed, mCollapseInterpolator, getPxVal(R.dimen.opa_line_x_collapse_ry), COLLAPSE_ANIMATION_DURATION_RY);
@@ -302,7 +302,7 @@ public class OpaLayout extends FrameLayout implements ButtonDispatcher.ButtonInt
         set.add(getScaleAnimatorX(mRed, 1.0f, DOTS_RESIZE_DURATION, mDotsFullSizeInterpolator));
         set.add(getScaleAnimatorY(mRed, 1.0f, DOTS_RESIZE_DURATION, mDotsFullSizeInterpolator));
         Animator animator2;
-        if (mIsLandscape) {
+        if (!mVertical) {
             animator2 = getDeltaAnimatorY(mBlue, mCollapseInterpolator, -getPxVal(R.dimen.opa_line_x_collapse_bg), COLLAPSE_ANIMATION_DURATION_BG);
         } else {
             animator2 = getDeltaAnimatorX(mBlue, mCollapseInterpolator, getPxVal(R.dimen.opa_line_x_collapse_bg), COLLAPSE_ANIMATION_DURATION_BG);
@@ -311,7 +311,7 @@ public class OpaLayout extends FrameLayout implements ButtonDispatcher.ButtonInt
         set.add(getScaleAnimatorX(mBlue, 1.0f, DOTS_RESIZE_DURATION, mDotsFullSizeInterpolator));
         set.add(getScaleAnimatorY(mBlue, 1.0f, DOTS_RESIZE_DURATION, mDotsFullSizeInterpolator));
         Animator animator3;
-        if (mIsLandscape) {
+        if (!mVertical) {
             animator3 = getDeltaAnimatorY(mYellow, mCollapseInterpolator, getPxVal(R.dimen.opa_line_x_collapse_ry), COLLAPSE_ANIMATION_DURATION_RY);
         } else {
             animator3 = getDeltaAnimatorX(mYellow, mCollapseInterpolator, -getPxVal(R.dimen.opa_line_x_collapse_ry), COLLAPSE_ANIMATION_DURATION_RY);
@@ -320,7 +320,7 @@ public class OpaLayout extends FrameLayout implements ButtonDispatcher.ButtonInt
         set.add(getScaleAnimatorX(mYellow, 1.0f, DOTS_RESIZE_DURATION, mDotsFullSizeInterpolator));
         set.add(getScaleAnimatorY(mYellow, 1.0f, DOTS_RESIZE_DURATION, mDotsFullSizeInterpolator));
         Animator animator4;
-        if (mIsLandscape) {
+        if (!mVertical) {
             animator4 = getDeltaAnimatorY(mGreen, mCollapseInterpolator, getPxVal(R.dimen.opa_line_x_collapse_bg), COLLAPSE_ANIMATION_DURATION_BG);
         } else {
             animator4 = getDeltaAnimatorX(mGreen, mCollapseInterpolator, -getPxVal(R.dimen.opa_line_x_collapse_bg), COLLAPSE_ANIMATION_DURATION_BG);
@@ -381,7 +381,7 @@ public class OpaLayout extends FrameLayout implements ButtonDispatcher.ButtonInt
 
     private ArraySet<Animator> getLineAnimatorSet() {
         final ArraySet<Animator> set = new ArraySet<Animator>();
-        if (mIsLandscape) {
+        if (!mVertical) {
             set.add(getDeltaAnimatorY(mRed, mFastOutSlowInInterpolator, getPxVal(R.dimen.opa_line_x_trans_ry), LINE_ANIMATION_DURATION_Y));
             set.add(getDeltaAnimatorX(mRed, mFastOutSlowInInterpolator, getPxVal(R.dimen.opa_line_y_translation), LINE_ANIMATION_DURATION_X));
             set.add(getDeltaAnimatorY(mBlue, mFastOutSlowInInterpolator, getPxVal(R.dimen.opa_line_x_trans_bg), LINE_ANIMATION_DURATION_Y));
@@ -584,24 +584,20 @@ public class OpaLayout extends FrameLayout implements ButtonDispatcher.ButtonInt
     public void setImageResource(int resId) {
         ((ImageView) mWhite).setImageResource(resId);
     }
-	
-	public void setVertical(boolean vertical) {
-		setLandscape(!vertical);
-	}
 
-    public void setLandscape(boolean landscape) {
-        mIsLandscape = landscape;
-        if (mIsLandscape) {
-            mTop = mGreen;
-            mBottom = mBlue;
-            mRight = mYellow;
-            mLeft = mRed;
-            return;
+    public void setVertical(boolean vertical) {
+        mVertical = vertical;
+        if (mVertical) {
+            mTop = mRed;
+            mBottom = mYellow;
+            mLeft = mBlue;
+            mRight = mGreen;
+             return;
         }
-        mTop = mRed;
-        mBottom = mYellow;
-        mLeft = mBlue;
-        mRight = mGreen;
+        mTop = mGreen;
+        mBottom = mBlue;
+        mRight = mYellow;
+        mLeft = mRed;
     }
 
     public void setOnLongClickListener(View.OnLongClickListener l) {
