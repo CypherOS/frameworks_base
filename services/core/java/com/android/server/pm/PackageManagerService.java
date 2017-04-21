a32c25b4f93ba944e7350078aa9b8e8499f65944
Chris <chris@cypheros.co>
2017-04-20 21:24:50 -0400

    servcies: Remove variables not supported by OMS
    Change-Id: Ic272c29897ecf138aedcc6ee511a864f0acaa863

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -2353,7 +2353,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                 scanDirTracedLI(new File(VENDOR_OVERLAY_DIR, overlayThemeDir), mDefParseFlags
                         | PackageParser.PARSE_IS_SYSTEM
                         | PackageParser.PARSE_IS_SYSTEM_DIR
-                        | PackageParser.PARSE_TRUSTED_OVERLAY, scanFlags | SCAN_TRUSTED_OVERLAY, 0);
+                        scanFlags, 0);
             }
             scanDirTracedLI(new File(VENDOR_OVERLAY_DIR), mDefParseFlags
                     | PackageParser.PARSE_IS_SYSTEM
