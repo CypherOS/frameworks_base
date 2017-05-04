e5332f292ed08164be28e5f0b38ed2571b736d63
Chris <chris@cypheros.co>
2017-05-04 12:45:52 -0400

    base: Cleanup navbar logic
    Change-Id: I4ec78fcaca4a7170892332e45664e838cefbc38e

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -655,8 +655,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         public void update() {
             int showNavBar = Settings.System.getIntForUser(
                     mContext.getContentResolver(), Settings.System.NAVIGATION_BAR_SHOW,
-                    0, mCurrentUserId);
-            if (showNavBar != 0){
+                    -1, mCurrentUserId);
+            if (showNavBar != -1){
                 boolean showNavBarBool = showNavBar == 1;
                 if (showNavBarBool !=  mShowNavBar){
                     updateNavigationBar();
