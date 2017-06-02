f5fb2bc3e9e58eceaf73a36108b0a5755a25315e
Anas Karbila <anaskarbila@gmail.com>
2017-06-03 07:34:05 +0900

    PowerManagerService: cut button lights duration in half
    * 5 seconds is way too long
    
    Change-Id: Ifb738bb532b88af6a87bf6775098bccb234fc22a

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -174,7 +174,7 @@ public final class PowerManagerService extends SystemService
     private static final int DEFAULT_DOUBLE_TAP_TO_WAKE = 0;
 
     // Default value for buttons lights timeout
-    private static final int BUTTON_ON_DURATION = 5000;
+    private static final int BUTTON_ON_DURATION = 2500;
 
     /** Constants for {@link #shutdownOrRebootInternal} */
     @Retention(RetentionPolicy.SOURCE)
