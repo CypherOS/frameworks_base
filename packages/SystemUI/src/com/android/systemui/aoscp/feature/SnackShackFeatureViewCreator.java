9695ac8386c99be7ed784917b07a5753bb8a9ad9
Chris <chris@cypheros.co>
2017-06-06 04:33:50 -0400

    base: Introduce Snackshack dialog system
    Snackshack is a snackbar based dialog merged
    between OTS by PA and a custom dialog held for
    user information system-wide.
    
    Change-Id: I0a10898683726a50cc88ccc4a6acbd4fde3c7e1f
    Note: Commit message will be updated when complete

---
+++ packages/SystemUI/src/com/android/systemui/aoscp/feature/SnackShackFeatureViewCreator.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2016 ParanoidAndroid Project
+ * Copyright 2017 CypherOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.aoscp.feature;
+
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.view.WindowManager;
+
+import com.android.systemui.R;
+
+/**
+ * SnackShack view creator for use with OnTheSpot & SnackShackFeature.
+ */
+public class SnackShackFeatureViewCreator {
+
+    private Context mContext;
+    private SnackShackFeatureView mSnackshackView;
+    private WindowManager mWindowManager;
+
+    public SnackShackFeatureViewCreator(Context context) {
+        mContext = context;
+        mWindowManager = (WindowManager) mContext.getSystemService(
+                Context.WINDOW_SERVICE);
+    }
+
+    private void initSnackShack() {
+        mSnackshackView = (SnackShackFeatureView) View.inflate(mContext,
+                R.layout.snack_shack_feature, null);
+        if (mSnackshackView != null) attachSnackShack();
+    }
+
+    private void attachSnackShack() {
+        if (mSnackshackView != null) {
+            final WindowManager.LayoutParams snackshackLp = new WindowManager.LayoutParams(
+                    LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.TYPE_NAVIGATION_BAR_PANEL,
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                    PixelFormat.TRANSLUCENT);
+            snackshackLp.gravity = Gravity.BOTTOM;
+            mWindowManager.addView(mSnackshackView, snackshackLp);
+        }
+    }
+
+    public SnackShackFeatureView getSnackshackView() {
+        initSnackShack();
+        return mSnackshackView;
+    }
+}
