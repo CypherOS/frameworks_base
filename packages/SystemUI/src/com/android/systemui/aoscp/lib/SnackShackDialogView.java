efe27642612e77d5cb244037acc5bcc5fa9c830a
Chris <chris@cypheros.co>
2017-06-06 01:40:55 -0400

    base: Introduce Snackshack dialog system
    Snackshack is a snackbar based dialog merged
    between OTS by PA and a custom dialog held for
    user information system-wide.
    
    Change-Id: I0a10898683726a50cc88ccc4a6acbd4fde3c7e1f
    Note: Commit message will be updated when complete

---
+++ packages/SystemUI/src/com/android/systemui/aoscp/lib/SnackShackDialogView.java
@@ -0,0 +1,270 @@
+/*
+ * Copyright 2016 ParanoidAndroid Project
+ * Copyright 2017 CypherOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.aoscp.lib;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.android.systemui.R;
+import com.android.systemui.aoscp.lib.SnackShackDialogHelper;
+
+/**
+ * Snackshack view for use with OnTheSpot & SnackShackDialog.
+ */
+public class SnackShackDialogView extends RelativeLayout {
+
+    /** Whether to output debugging information to logs. */
+    private static final boolean DEBUG = SnackShackDialogHelper.DEBUG;
+    /** Log output tag. */
+    private static final String LOG_TAG = SnackShackDialogHelper.LOG_TAG;
+
+    /** Amount of milliseconds for how long the animations should last. */
+    private static final int ANIMATION_DURATION = 250;
+    /** Amount of milliseconds for how long to show the snackshack view. */
+    private static final int TIMEOUT_DURATION = 30000;
+
+    /** Listener for user choice buttons. */
+    private final View.OnClickListener mButtonListener = new OnClickListener() {
+
+        @Override
+        public void onClick(final View v) {
+            if (v.equals(mConfirmButton)) {
+                if (mCallbackHandler != null) {
+                    mCallbackHandler.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            if (mCallback != null && mSettingName != null) {
+                                mCallback.onSettingConfirm(mSettingName);
+                            }
+                            mCallback = null;
+                            mSettingName = null;
+                        }
+
+                    });
+                    mCallbackHandler = null;
+                }
+
+                hide();
+            }
+
+            if (v.equals(mDenyButton)) {
+                if (mCallbackHandler != null) {
+                    mCallbackHandler.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            if (mCallback != null && mSettingName != null) {
+                                mCallback.onSettingDeny(mSettingName);
+                            }
+                            mCallback = null;
+                            mSettingName = null;
+                        }
+
+                    });
+                    mCallbackHandler = null;
+                }
+
+                hide();
+            }
+			
+			if (v.equals(mInfoButton)) {
+                if (mCallbackHandler != null) {
+                    mCallbackHandler.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            if (mCallback != null) {
+                                mCallback.onSettingInfo();
+                            }
+                            mCallback = null;
+                        }
+
+                    });
+                    mCallbackHandler = null;
+                }
+
+                hide();
+            }
+        }
+
+    };
+
+    /** Runnable for scheduling hiding. */
+    private final Runnable mHideRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            hide();
+        }
+
+    };
+
+    /** Main handler to do any serious main work in. Fallback for callbacks. */
+    private final Handler mMainHandler;
+
+	/** Dialog title of the main objective. */
+    private TextView mTitle = null;
+	
+    /** Description text view for informing the user. */
+    private TextView mDescription = null;
+
+    /** User choice button for confirming the action. */
+    private View mConfirmButton = null;
+
+    /** User choice button for denying the action. */
+    private View mDenyButton = null;
+	
+	/** User confirmation button after reading dialog. */
+    private View mInfoButton = null;
+
+    /** Setting name that is being confirmed. To be passed back to the callback. */
+    private String mSettingName = null;
+
+    /** Callback to call upon user choice. */
+    private SnackShackDialogHelper.OnSettingChoiceListener mCallback = null;
+
+    /** Handler to send callbacks on. */
+    private Handler mCallbackHandler = null;
+
+    /**
+     * Constructs the snackshack view object.
+     *
+     * @param context  {@link Context} the view is going to run in
+     * @param attrs  {@link AttributeSet} of the XML tag that is inflating the view
+     */
+    public SnackShackDialogView(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+        mMainHandler = new Handler(Looper.getMainLooper());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        setTranslationY(getHeight());
+        setVisibility(View.GONE);
+
+		mTitle = (TextView) findViewById(R.id.title);
+        mDescription = (TextView) findViewById(R.id.description);
+
+        mConfirmButton = findViewById(R.id.action_confirm);
+        mConfirmButton.setOnClickListener(mButtonListener);
+        mConfirmButton.setClickable(true);
+
+        mDenyButton = findViewById(R.id.action_deny);
+        mDenyButton.setOnClickListener(mButtonListener);
+        mDenyButton.setClickable(true);
+		
+		mInfoButton = findViewById(R.id.action_info);
+        mInfoButton.setOnClickListener(mButtonListener);
+        mInfoButton.setClickable(true);
+    }
+
+    /**
+     * Shows the snackshack.
+     *
+     * @param settingName  {@link String} name of the {@link Settings.Secure} being changed
+	 * @param title  {@link String} title that objects the message
+     * @param message  {@link String} message to display to the user
+     * @param listener  {@link SnackShackDialogHelper.OnSettingChoiceListener} to notify
+     *                  about the choice
+     * @param handler  {@link Handler} to notify the listener on,
+     *                 or null to notify it on the UI thread instead
+     */
+    public void show(final String settingName, final String title, final String message,
+            final SnackShackDialogHelper.OnSettingChoiceListener callback,
+            Handler handler) {
+        if (settingName == null) {
+            throw new IllegalArgumentException("settingName == null");
+        }
+		if (title == null) {
+            throw new IllegalArgumentException("title == null");
+        }
+        if (message == null) {
+            throw new IllegalArgumentException("message == null");
+        }
+        if (callback == null) {
+            throw new IllegalArgumentException("callback == null");
+        }
+        if (handler == null) {
+            handler = mMainHandler;
+        }
+
+        if (DEBUG) Log.d(LOG_TAG, "Showing the snackshack view");
+
+        mSettingName = settingName;
+	    mTitle.setText(title);
+        mDescription.setText(message);
+        mCallback = callback;
+        mCallbackHandler = handler;
+
+        animate().translationY(getHeight())
+                .setInterpolator(AnimationUtils.loadInterpolator(getContext(),
+                        android.R.interpolator.fast_out_slow_in))
+                .setDuration(ANIMATION_DURATION)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationStart(final Animator animation) {
+                        setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(final Animator animation) {
+                        animate().translationY(0f)
+                                .setInterpolator(AnimationUtils.loadInterpolator(getContext(),
+                                        android.R.interpolator.fast_out_slow_in))
+                                .setDuration(ANIMATION_DURATION)
+                                .start();
+                    }
+                }).start();
+        }
+
+        mMainHandler.removeCallbacks(mHideRunnable);
+        mMainHandler.postDelayed(mHideRunnable, TIMEOUT_DURATION);
+    }
+
+    /**
+     * Hides the snackshack.
+     */
+    public void hide() {
+        if (DEBUG) Log.d(LOG_TAG, "Hiding the snackshack view");
+
+        animate().translationY(getHeight())
+                .setInterpolator(AnimationUtils.loadInterpolator(getContext(),
+                        android.R.interpolator.fast_out_slow_in))
+                .setDuration(ANIMATION_DURATION)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(final Animator animation) {
+                        setVisibility(View.GONE);
+                    }
+                }).start();
+    }
+
+}
