baf3f666de344f9d23b6a849cf7e4bf3bd76201f
beanstown106 <nbenis106@gmail.com>
2017-06-14 22:52:59 -0400

    base: allow home button animation to be turned off
    Change-Id: Ia92ec926324dfe727a4e2224b2159ed71561d0b6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3362,6 +3362,12 @@ public final class Settings {
                 "notification_light_screen_on_enable";
 
         /**
+         * Whether to enable the pixel navbar animation
+         * @hide
+         */
+        public static final String PIXEL_NAV_ANIMATION = "pixel_nav_animation";
+
+        /**
          * Whether the notification LED should repeatedly flash when a notification is
          * pending. The value is boolean (1 or 0).
          * @hide
