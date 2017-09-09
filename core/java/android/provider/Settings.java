7cd1ad5f3add9de97ce348487db5ab235881a569
Chris <chris@cypheros.co>
2017-09-09 00:17:07 -0400

    SystemUI: add double tap to sleep gesture
    - Make it more reliable
    - Add it to keyguard
    - Add a content observer to not always query Settings.System on every
    touch event
    
    Change-Id: Icec46df8bc964bf2f51d2d76ad98546f01fe1d92

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3945,6 +3945,12 @@ public final class Settings {
         public static final String POCKET_JUDGE = "pocket_judge";
 
         /**
+         * Enable statusbar double tap gesture on to put device to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
