0e33cc75cba81af1976e67d4434bd21faf6b2976
Park Ju Hyung <qkrwngud825@gmail.com>
2017-10-30 21:10:01 -0400

    SystemUI: Implement burn-in protection for status-bar/nav-bar items
    Devices with OLED display suffer from
    status-bar's notification items and nagivation bar's software keys
    causing permanent burn-ins when used long-term.
    
    Moving all items in the area
    both horizontally and vertically workarounds this problem.
    
    This new feature can be enabled by setting
    config_statusBarBurnInProtection to true.
    
    The shifting interval can be configured with config_shift_interval.
    The default shifting interval is 60 seconds.
    
    Updated for Oreo by @Sirhc95
    - Updated class member to mNavigationBarViewRoot to avoid conflict with
      pre-existing members in oreo.
    
    Change-Id: Ibcf78a3b7cade8454629377d8527e94ebb805865
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Thecrazyskull <anaskarbila@gmail.com>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -212,6 +212,7 @@ import com.android.systemui.statusbar.phone.UnlockMethodCache.OnUnlockMethodChan
 import com.android.systemui.statusbar.policy.BatteryController;
 import com.android.systemui.statusbar.policy.BatteryController.BatteryStateChangeCallback;
 import com.android.systemui.statusbar.policy.BrightnessMirrorController;
+import com.android.systemui.statusbar.policy.BurnInProtectionController;
 import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.statusbar.policy.ConfigurationController.ConfigurationListener;
 import com.android.systemui.statusbar.policy.DarkIconDispatcher;
@@ -403,6 +404,9 @@ public class StatusBar extends SystemUI implements DemoMode,
     protected FingerprintUnlockController mFingerprintUnlockController;
     LightBarController mLightBarController;
     protected LockscreenWallpaper mLockscreenWallpaper;
+	
+	private BurnInProtectionController mBurnInProtectionController;
+	private NavigationBarView mNavigationBarViewRoot;
 
     int mNaturalBarHeight = -1;
 
@@ -1082,6 +1086,8 @@ public class StatusBar extends SystemUI implements DemoMode,
                     R.id.header_debug_info);
             mNotificationPanelDebugText.setVisibility(View.VISIBLE);
         }
+		
+		mBurnInProtectionController = new BurnInProtectionController(mContext, mStatusBarView);
 
         try {
             boolean showNav = mWindowManagerService.hasNavigationBar();
@@ -1642,6 +1648,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         } else {
             removeNavigationBar();
         }
+		mBurnInProtectionController.setNavigationBarView(enabled ? mNavigationBarViewRoot : null);
     }
 
     private void resetSystemUIVisibility() {
@@ -5024,6 +5031,9 @@ public class StatusBar extends SystemUI implements DemoMode,
         mStackScroller.setAnimationsEnabled(false);
         mVisualStabilityManager.setScreenOn(false);
         updateVisibleToUser();
+		if (mBurnInProtectionController != null) {
+            mBurnInProtectionController.stopSwiftTimer();
+        }
 
         // We need to disable touch events because these might
         // collapse the panel after we expanded it, and thus we would end up with a blank
@@ -5050,6 +5060,9 @@ public class StatusBar extends SystemUI implements DemoMode,
         mVisualStabilityManager.setScreenOn(true);
         mNotificationPanel.setTouchDisabled(false);
         updateVisibleToUser();
+		if (mBurnInProtectionController != null) {
+            mBurnInProtectionController.startSwiftTimer();
+        }
     }
 
     public void onScreenTurningOn() {
