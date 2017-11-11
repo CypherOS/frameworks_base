82e44e7251e744b08b1523904312f5cd646b8b52
Chris <chris@cypheros.co>
2017-11-10 21:40:53 -0500

    SystemUI: add new brightness slider UI
    This commit adds a new look for the brightness slider in QuickSettings.
    
    - Brightness slider is now at the footer of the QS
    - Auto brightness toggle is now beside the brightness slider (Users can disable this)
    - Brightness slider has indicators for min and max
    
    This is a squash of:
    
    1. Add auto brightness toggle to Quick Settings
       Author: maxwen <max.weninger@gmail.com>
       Date: Sun Jan 3 11:52:48 2016 +0530
       5 files changed, 48 insertions(+), 7 deletions(-)
    
    2. Add option to disable auto brightness icon in brightness slider
       Author: Varun <date.varun123@gmail.com>
       Date: Thu Jan 7 21:52:27 2016 +0530
       2 files changed, 21 insertions(+), 3 deletions(-)
    
    3. QS auto brightness toggle rework
       Author: maxwen <max.weninger@gmail.com>
       Date: Sun Jan 3 17:11:54 2016 +0100
       5 files changed, 64 insertions(+), 12 deletions(-)
    
    4. Fix automatic brightness QS toggle icon
       Author: SpiritCroc <spiritcroc@gmail.com>
       Date: Mon Oct 31 10:55:05 2016 +0100
       1 file changed, 1 insertion(+), 1 deletion(-)
    
    5. Only enable auto brightness toggle when also enabed for QS
       Author: SpiritCroc <spiritcroc@gmail.com>
       Date: Mon Oct 31 11:11:45 2016 +0100
       1 file changed, 6 insertions(+), 2 deletions(-)
    
    6. Fix brightness icon not showing
       Author: SpiritCroc <spiritcroc@gmail.com>
       Date: Fri Aug 11 22:45:24 2017 +0800
       1 file changed, 13 insertions(+), 11 deletions(-)
    
    7. Brightness control on footer
       Author: jhenrique09 <jhenrique09.mcz@hotmail.com>
       Date: Fri Oct 20 12:38:44 2017 -0200
       7 files changed, 37 insertions(+), 11 deletions(-)
    
    8. Rework auto brightness implementation
       Author: JBCDroid2 <jonathan.lyndo@outlook.com>
       Date: Wed Nov 1 10:50:14 2017 -0400
       4 files changed, 118 insertions(+), 38 deletions(-)
    
    9. Make use of the brightness slider buttons
       Author: Alex Cruz <du.alexcruz@gmail.com>
       Date: Sun Nov 5 21:16:22 2017 -0500
       1 file changed, 84 insertions(+), 4 deletions(-)
    
    10. Footer brightness slider: fix FC and code cleanup
        Author: ezio84 <brabus84@gmail.com>
        Date: Wed Nov 8 11:36:53 2017 +0100
        1 file changed, 22 insertions(+), 40 deletions(-)
    
    11. Brightness slider buttons: make them dynamic (auto and manual)
        Author: ezio84 <brabus84@gmail.com>
        Date: Wed Nov 8 15:06:42 2017 +0100
        1 file changed, 60 insertions(+), 28 deletions(-)
    
    12. Footer brightness buttons: make touch area more reliable
        Author: ezio84 <brabus84@gmail.com>
        Date: Wed Nov 8 16:08:16 2017 +0100
        5 files changed, 52 insertions(+), 75 deletions(-)
    
    13. Footer brightness slider: improve hide/show animation
        Author: ezio84 <brabus84@gmail.com>
        Date: Wed Nov 8 21:00:19 2017 +0100
        1 file changed, 14 insertions(+), 6 deletions(-)
    
    Change-Id: If75e27fa2430be7679b9c4cc2d440f1f06b1ac62

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4128,6 +4128,12 @@ public final class Settings {
         public static final String FAST_CHARGING_LED_ENABLED = "fast_charging_led_enabled";
 
         /**
+         * Whether to show Brightness Icon On Brightness Slider
+         * @hide
+         */
+        public static final String QS_SHOW_BRIGHTNESS_ICON = "qs_show_brightness_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
