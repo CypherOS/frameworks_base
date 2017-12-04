e1bbabdd75c86438edc362349d78201e485f080a
Chris <chris@cypheros.co>
2017-12-03 21:26:38 -0500

    SystemUI: Introduce Extended Restart
    - Adpated to the new powermenu in 8.1
    
    Change-Id: Ibaf918f55c973738b8a47678cf9c4f8f187cdbae
    PS1: Do not merge, this is incomplete

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsImpl.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsImpl.java
@@ -62,7 +62,8 @@ public class GlobalActionsImpl implements GlobalActions {
     }
 
     @Override
-    public void showShutdownUi(boolean isReboot, String reason) {
+    public void showShutdownUi(boolean isReboot, boolean isRebootBootloader,
+                boolean isRebootRecovery, String reason) {
         GradientDrawable background = new GradientDrawable(mContext);
         background.setAlpha((int) (SHUTDOWN_SCRIM_ALPHA * 255));
 
@@ -97,6 +98,8 @@ public class GlobalActionsImpl implements GlobalActions {
         TextView message = d.findViewById(R.id.text1);
         message.setTextColor(color);
         if (isReboot) message.setText(R.string.reboot_to_reset_message);
+		if (isRebootBootloader) message.setText(R.string.global_restart_bootloader_message);
+		if (isRebootRecovery) message.setText(R.string.global_restart_recovery_message);
 
         Point displaySize = new Point();
         mContext.getDisplay().getRealSize(displaySize);
