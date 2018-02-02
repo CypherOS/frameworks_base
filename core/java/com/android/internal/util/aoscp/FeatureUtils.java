55764920768200cb4303752925a43b82467cbba5
Chris <chris@cypheros.co>
2018-02-02 02:08:40 -0500

    FeatureUtils: Add ability to determine flashlight support
    Reads the device to determine whether a flashlight is available.
    
    Pulled from:
    
    https://github.com/DirtyUnicorns/android_external_DUtils/blob/o8x/src/com/android/internal/utils/du/DUActionUtils.java
    
    Change-Id: I1fc8dd56f7ce568e138a47ad0674911d40dfe9c5

--- core/java/com/android/internal/util/aoscp/FeatureUtils.java
+++ core/java/com/android/internal/util/aoscp/FeatureUtils.java
@@ -17,6 +17,10 @@
 
 package com.android.internal.util.aoscp;
 
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
 import android.Manifest;
 import android.os.RemoteException;
 import android.os.ServiceManager;
@@ -25,6 +29,29 @@ import com.android.internal.R;
 import com.android.internal.statusbar.IStatusBarService;
 
 public class FeatureUtils {
+	
+	// Determines whether the device has a flashlight
+	public static boolean deviceSupportsFlashLight(Context context) {
+        CameraManager cameraManager = (CameraManager) context.getSystemService(
+                Context.CAMERA_SERVICE);
+        try {
+            String[] ids = cameraManager.getCameraIdList();
+            for (String id : ids) {
+                CameraCharacteristics c = cameraManager.getCameraCharacteristics(id);
+                Boolean flashAvailable = c.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+                Integer lensFacing = c.get(CameraCharacteristics.LENS_FACING);
+                if (flashAvailable != null
+                        && flashAvailable
+                        && lensFacing != null
+                        && lensFacing == CameraCharacteristics.LENS_FACING_BACK) {
+                    return true;
+                }
+            }
+        } catch (CameraAccessException | AssertionError e) {
+            // Ignore
+        }
+        return false;
+    }
 
     // Toggle flashlight
     public static void toggleFlashLight() {
