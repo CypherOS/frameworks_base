8f79e766d1af069b310d2d368d6d71566d3f7475
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2018-02-20 13:53:55 -0500

    base: Improvements for screenrecord
    Supports switching between 3 different resolutions:
    
    - 480*800 resolution and 1.5Mbps
    - default resolution (720p) and 4Mbps
    - default resolution (720p) and 8Mbps
    
    Also improve the screenrecord service and be sure
    to stop it when we are done.
    
    @Sirhc95: This does not include the blinking red light or the tile swapping
    logic. Instead, i added system setting support for the user to change resolutions
    from settings, and single clicking on the tile will trigger screenrecord for
    whatever selected setting.
    
    Also added a new screenrecord icon
    
    Change-Id: Iee7484df275963cd37ed817cb8ab63e9b6d91242

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -39,6 +39,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.ScreenrecordTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
@@ -75,6 +76,7 @@ public class QSFactoryImpl implements QSFactory {
         else if (tileSpec.equals("screenshot")) return new ScreenshotTile(mHost);
         else if (tileSpec.equals("alertslider")) return new AlertSliderTile(mHost);
         else if (tileSpec.equals("caffeine")) return new CaffeineTile(mHost);
+        else if (tileSpec.equals("screenrecord")) return new ScreenrecordTile(mHost);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(mHost, tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(mHost, tileSpec);
