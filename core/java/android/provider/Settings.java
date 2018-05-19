7b0dbb096c657324d82388b242fde79578183db8
Chris <chris@cypheros.co>
2018-05-18 20:46:18 -0400

    Introduce Ambient Indication Interface
    The is a interface that handles subtle indications in Ambient Display
    and the Lockscreen. To start, we've rewritten our Ambient Play feature
    from scratch to be housed in this interface.
    
    Ambient Play:
    
    A music recognition service that records data from the users surroundings,
    to then submit that data to a fingerprint matching database. If a match
    is found, it returns a result. That result is then parsed and display via
    the AmbientIndicationContainer for Ambient Display and Lockscreen, as well
    as a non-intrusive notification.
    
    Some of the improvements we've made from our previous iteration is battery
    and performance. Ambient Play now listens for data every 120 seconds instead
    of 60 seconds. Ambient Play also auto disables when a device is charging.
    This will prevent possible slow charging and rare discharging due to the
    amount of background proccessing.
    
    In our previous iteration, the recording thread wasn't stopping properly
    and would almost always run continuosly, even when the user disabled the
    feature. We've added an extensive ammount of calls to stop the thread after
    multiple events, to insure the thread only runs to serve it's one purpose
    then goes away.
    
    test: Battery and performance is not affected as much as before.
    
    Ambient Container:
    
    Developed by Google and backported by frap129 although we're using a
    slimmed down version of the container from ezio84
    
    todo:
    
    - Add double tap events, to submit data to Google Search
    - Add support for displaying history from matched results in Settings
    - Add Ambient Weather
    
    Change-Id: I8f8d90367bf2c538a17828cb445859eeb4f0b540
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7892,6 +7892,20 @@ public final class Settings {
         public static final String SYSTEM_ACCENT = "system_accent";
 
         /**
+         * Whether Ambient Play enabled/disabled
+         *
+         * @hide
+         */
+        public static final String AMBIENT_RECOGNITION = "ambient_recognition";
+
+        /**
+         * Whether Ambient Play is shown on the lockscreen
+         *
+         * @hide
+         */
+        public static final String AMBIENT_RECOGNITION_KEYGUARD = "ambient_recognition_keyguard";
+
+        /**
          * Whether or not the automatic storage manager is enabled and should run on the device.
          *
          * @hide
