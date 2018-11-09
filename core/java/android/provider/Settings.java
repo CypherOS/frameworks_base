94be8da9020cd0f30e176f48f504700bfe77b8bc
Chris <chris@cypheros.co>
2018-11-08 20:56:49 -0500

    onehand: Disable onehanded mode when using Quickstep
    - Move the feature enablement setting to global system
    - Added method to determine Quickstep availability and return the status
    of onehanded mode based on it
    
    The reason for this is onehanded mode does not function properly whilst
    using swipe gestures in Quickstep. Quick swipes (such as launching the
    last app) will trigger onehanded mode in most cases.
    
    Change-Id: Iba0fcafe0de4cd9e316cf119b6f59120ee54c4f1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4310,6 +4310,12 @@ public final class Settings {
          * @hide
          */
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+		
+		/**
+         * Enable OneHanded Mode
+         * @hide
+         */
+        public static final String ONEHANDED_MODE = "onehanded_mode";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
@@ -4503,6 +4509,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(AMBIENT_RECOGNITION_KEYGUARD);
             PRIVATE_SETTINGS.add(AMBIENT_RECOGNITION_NOTIFICATION);
             PRIVATE_SETTINGS.add(WEATHER_LOCKSCREEN_UNIT);
+			PRIVATE_SETTINGS.add(ONEHANDED_MODE);
         }
 
         /**
