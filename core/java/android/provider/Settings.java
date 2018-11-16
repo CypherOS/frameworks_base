619bed1aa58fdcec4cfd768128d170524e4c184e
Chris <chris@cypheros.co>
2018-11-16 15:14:23 -0500

    core: Add device intro settings intent
    Other packages can use this intent to launch the device introduction
    
    Change-Id: If4aa9a5d11967a8c4b222e187901d44d220e23dd

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1579,6 +1579,20 @@ public final class Settings {
     public static final String ACTION_STORAGE_VOLUME_ACCESS_SETTINGS =
             "android.settings.STORAGE_VOLUME_ACCESS_SETTINGS";
 
+	/**
+     * Activity Action: Show first time device intro 
+     * <p>
+     * In some cases, a matching Activity may not exist, so ensure you
+     * safeguard against this.
+     * <p>
+     * Input: Nothing.
+     * <p>
+     * Output: Nothing
+     */
+    @SdkConstant(SdkConstantType.ACTIVITY_INTENT_ACTION)
+    public static final String ACTION_DEVICE_INTRODUCTION =
+        "android.settings.DEVICE_INTRODUCTION";
+
     // End of Intent actions for Settings
 
     /**
