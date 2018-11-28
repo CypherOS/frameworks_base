eff25726b42df5b5c6fbe49241ef562c5ce32c0d
Chris <chris@cypheros.co>
2018-11-28 05:51:49 -0500

    policy: Fingerprint navigation improvements
    Switch to HwBinder transactions that communicate directly with the
    biometrics hal. This allows statuses to change faster, without the need
    to restart the hal
    
    Based on
    https://github.com/berkeley-dev/android_device_huawei_kirin970-common/commits/lineage-16.0/lineagehw/src/org/lineageos/hardware/ExtBiometricsFingerprint.java
    
    Change-Id: I5041d7a6ffd9ab4b81b5a56d978926b6dddba3ca

---
+++ core/java/android/hardware/biometrics/BiometricTransactor.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2018 CypherOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.hardware.biometrics;
+
+
+/**
+ * Class that applies hardware transactions to the
+ * biometrics hal.
+ * @hide
+ */
+public class BiometricTransactor {
+    
+	int BIOMETRIC_NAVIGATION_ENABLE = 41;
+
+	int BIOMETRIC_NAVIGATION_DISABLE = 42;
+	
+	private static final String TAG = "BiometricTransactor";
+
+	private static String mDescriptor;
+    private static int mTransactionId;
+	
+	private static IHwBinder sBiometricTransactor;
+	
+	public BiometricTransactor(Context context) throws RemoteException {
+        mDescriptor = context.getResources().getString(
+                R.string.config_vendorBiometricsDescriptor);
+        mTransactionId = context.getResources().getInteger(
+                R.integer.config_vendorBiometricsTransactionId);
+        sBiometricTransactor = HwBinder.getService(mDescriptor, "default");
+    }
+
+	public int sendCmdToHal(int cmdId) {
+        if (sBiometricTransactor == null) {
+            return -1;
+        }
+
+		if (TextUtils.isEmpty(mDescriptor)) {
+            Log.d(TAG, "Descriptor returned null");
+            return -1;
+        }
+
+		if (mTransactionId == 0) {
+			Log.d(TAG, "Transaction ID returned null");
+			return -1
+		}
+		
+		HwParcel data = new HwParcel();
+        HwParcel reply = new HwParcel();
+
+		try {
+            data.writeInterfaceToken(mDescriptor);
+            data.writeInt32(cmdId);
+
+            sBiometricTransactor.transact(mTransactionId, data, reply, 0);
+			
+			reply.verifySuccess();
+            data.releaseTemporaryStorage();
+            return reply.readInt32();
+        } catch (Throwable t) {
+            return -1;
+		} finally {
+            reply.release();
+        }
+    }
+
+	public int sendMsgEnable() {
+		return BIOMETRIC_NAVIGATION_ENABLE;
+	}
+
+	public int sendMsgDisable() {
+		return BIOMETRIC_NAVIGATION_DISABLE;
+	}
+}
