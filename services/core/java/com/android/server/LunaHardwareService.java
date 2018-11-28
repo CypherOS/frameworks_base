49957acf28831b917ffe5cbfac13d4cd7d1d70b6
Chris <chris@cypheros.co>
2018-11-28 12:44:23 -0500

    Introduce DisplayEngine API
    Change-Id: I523f2332d14fec6ac884bf034e2a34ff82611526

---
+++ services/core/java/com/android/server/LunaHardwareService.java
@@ -0,0 +1,44 @@
+/* 
+ * Copyright (C) 2018 CypherOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server;
+
+import android.content.Context;
+
+import aoscp.hardware.DisplayEngineController;
+
+public class LunaHardwareService extends SystemService {
+    static final String TAG = "LunaHardwareService";
+    static final boolean DEBUG = false;
+
+    public LunaHardwareService(Context context) {
+        super(context);
+        mDisplayEngineController = new DisplayEngineController(context);
+    }
+
+    @Override
+    public void onStart() {
+        initHardwareControllers();
+    }
+
+    @Override
+    public void onBootPhase(int phase) {
+    }
+
+    private void initHardwareControllers() {
+		mDisplayEngineController.init();
+	}
+}
