bf85588ba944005fcacb2ffe610d8464ee0b1c76
Chris <chris@cypheros.co>
2018-11-29 03:02:01 -0500

    Introduce DisplayEngine API
    Change-Id: I523f2332d14fec6ac884bf034e2a34ff82611526

---
+++ services/core/java/com/android/server/DeviceHardwareService.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2018 CypherOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server;
+
+import android.content.Context;
+
+import aoscp.hardware.DisplayEngineController;
+
+public class DeviceHardwareService extends SystemService {
+    static final String TAG = "DeviceHardwareService";
+    static final boolean DEBUG = false;
+
+    private DisplayEngineController mDisplayEngineController;
+
+    public DeviceHardwareService(Context context) {
+        super(context);
+        mDisplayEngineController = new DisplayEngineController(context);
+    }
+
+    @Override
+    public void onStart() {
+        initHardwareControllers();
+    }
+
+    @Override
+    public void onBootPhase(int phase) {
+    }
+
+    private void initHardwareControllers() {
+        mDisplayEngineController.init();
+    }
+}
