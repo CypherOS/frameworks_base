02ed45058bc8258d23fa295aa3f51dbba163fa3d
TheStrix <parthbhatia98@gmail.com>
2019-01-05 19:38:25 +0530

    base: Implement battery styles (1/2)
    Original commits:
     Author: TheStrix <parthbhatia98@gmail.com>
     Date: Sat Jan 5 19:27:53 2019 +0530
     PA: Battery circle & bolt
     BatteryMeterDrawable: Add ParanoidAndroid customizations
      - Modify to PA circle battery icon
      - Add charging bolt icon
    
     Author: ezio84 <brabus84@gmail.com>
     Date: Fri Dec 14 19:10:33 2018 +0100
     Battery icon style: improve text/hidden switch [1/2]
        if the set style is text or hidden, don't allow
        percentage option (inside/outside icon) to break it
        also cleanup unused code
    
     Author: ezio84 <brabus84@gmail.com>
     Date: Wed Nov 21 22:31:39 2018 +0100
     Battery icon: force show pct in QS header and when charging/powersave
        like aosp already wanted to do (QuickStatusBarHeader setForceShowPercent
        was being overridden by the tuner observer added by custom battery
        commits to BatteryMeterView).
    
        This is also good for users that don't want the percentage in the
        statusbar but still want to be able to check it in the QS panel.
    
        PS 3: Fix percentage not showing in qs/keyguard after screen off / on
        and force it only when hidden icon style or hidden percentage option
        are enabled
    
     Author: ezio84 <brabus84@gmail.com>
     Date: Thu Nov 15 23:15:09 2018 +0100
     Battery icon: show outside percentage on charging
        for circle and portrait, the bolt overrides inside icon percentage
        same for power save
    
     Author: ezio84 <brabus84@gmail.com>
     Date: Wed Feb 14 00:24:18 2018 +0100
     Text battery style: show a ⚡when charging
    
     Author: ezio84 <brabus84@gmail.com>
     Date: Wed Nov 14 22:50:09 2018 +0100
     Battery icon: show pct on the right of the icon like stock
        and set the correct relative padding for the percentage
    
     Author: ezio84 <brabus84@gmail.com>
     Date: Wed Nov 21 21:31:02 2018 +0100
     Battery icon: circle styles fixes
      fix critical level exclamation mark position and size
      (to quickly test the issue just set mCriticalLevel to 100 in
      BatteryMeterDrawableBase)
    
      improve power saving "+" size so it doesn't look like a hospital symbol
    
     Author: Nicolas Dhouailly <ndhouailly@gmail.com>
     Date: Sun Nov 11 18:04:32 2018 +0100
     Battery styles : Fix icon portrait level
      * The icon portrait mode was always displaying full level. Lets fix it.
      * Clean up code a little bit.
    
     Author: Alex Cruz <alex@dirtyunicorns.com>
     Date: Wed Nov 7 12:28:39 2018 -0600
     Clean up battery styles
      - Remove code for landscape battery. Won't be used any time soon.
    
     Author: Simon Davie <nexx@nexxdesign.co.uk>
     Date: Mon Oct 24 02:12:18 2011 +0100
     BatteryMeterDrawable: Add hidden option
      * Remove the tuner call for icon blacklisting
        since it is no longer needed
    
     Author: Danesh M <daneshm90@gmail.com>
     Date: Thu Nov 27 14:17:19 2014 +0100
     BatteryMeterDrawable: Add text style
    
     Author: Danesh M <daneshm90@gmail.com>
     Date: Thu Nov 27 14:17:19 2014 +0100
     BatteryMeterDrawable: Add battery styles
      - Portrait
      - Landscape
      - Circle
      - Battery percent views (Inside/Ouside/Hidden)
    
    Change-Id: I7af333d4b8bede5643660a3e4888950e178b525f
    Signed-off-by: TheStrix <parthbhatia98@gmail.com>
    Signed-off-by: Hernán Castañón <herna@paranoidandroid.co>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4093,13 +4093,15 @@ public final class Settings {
         /**
          * Setting to determine whether or not to show the battery percentage in the status bar.
          *    0 - Don't show percentage
-         *    1 - Show percentage
+         *    1 - Show percentage outside the battery icon
+         *    2 - Show percentage inside the battery icon
          * @hide
          */
         public static final String SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
 
         /** @hide */
-        private static final Validator SHOW_BATTERY_PERCENT_VALIDATOR = BOOLEAN_VALIDATOR;
+        private static final Validator SHOW_BATTERY_PERCENT_VALIDATOR =
+                new SettingsValidators.InclusiveIntegerRangeValidator(0, 2);
 
         /**
          * Whether user can enable/disable navigation bar.
@@ -8270,6 +8272,29 @@ public final class Settings {
                 "packages_to_clear_data_before_full_restore";
 
         /**
+         * Paranoid Android SecureSettings additions starts
+         */
+
+        /**
+         * Display style of the status bar battery information
+         * 0: Display the battery an icon in portrait mode
+         * 1: Display the battery as a circle
+         * 2: Display the battery as text
+         * 3: Do not display the battery
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+
+        /** @hide */
+        private static final Validator STATUS_BAR_BATTERY_STYLE_VALIDATOR =
+                new SettingsValidators.InclusiveIntegerRangeValidator(0, 3);
+
+        /**
+         * Paranoid Android SecureSettings additions ends
+         */
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
@@ -8371,6 +8396,7 @@ public final class Settings {
             VOLUME_HUSH_GESTURE,
             MANUAL_RINGER_TOGGLE_COUNT,
             HUSH_GESTURE_USED,
+            STATUS_BAR_BATTERY_STYLE,
         };
 
         /**
@@ -8520,6 +8546,8 @@ public final class Settings {
             VALIDATORS.put(MANUAL_RINGER_TOGGLE_COUNT, MANUAL_RINGER_TOGGLE_COUNT_VALIDATOR);
             VALIDATORS.put(LOCK_SCREEN_ALLOW_PRIVATE_NOTIFICATIONS, BOOLEAN_VALIDATOR);
             VALIDATORS.put(LOCK_SCREEN_SHOW_NOTIFICATIONS, BOOLEAN_VALIDATOR);
+            // Paranoid Android additions starts
+            VALIDATORS.put(STATUS_BAR_BATTERY_STYLE , STATUS_BAR_BATTERY_STYLE_VALIDATOR);
         }
 
         /**
