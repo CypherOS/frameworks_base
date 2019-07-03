7ff53a654fac3d78b4a22550bc0a9622dbf37b1b
Chris Crump <chris@cypheros.co>
2019-07-03 19:28:53 -0400

    In-DisplayFingerprint: Update icon and add a separate one for enrolling
    Settings needs a darker icon when enrolling, otherwise it blends with 
    the background
    
    Change-Id: I2003d3c4125449f165d11ae62e72c6678755e609

--- packages/SystemUI/src/com/android/systemui/aoscp/fingerprint/InScreenFingerprint.java
+++ packages/SystemUI/src/com/android/systemui/aoscp/fingerprint/InScreenFingerprint.java
@@ -94,7 +94,9 @@ public class InScreenFingerprint extends ImageView implements OnTouchListener {
 
             new Handler(Looper.getMainLooper()).post(() -> {
                 setDim(false);
-                setImageResource(R.drawable.fingerprint_in_screen_default);
+                setImageResource(mIsEnrolling
+                    ? R.drawable.fingerprint_in_screen_enrolling
+                    : R.drawable.fingerprint_in_screen_default);
 
                 invalidate();
             });
@@ -186,7 +188,9 @@ public class InScreenFingerprint extends ImageView implements OnTouchListener {
         mPaintFingerprint.setAntiAlias(true);
         mPaintFingerprint.setColor(res.getColor(R.color.config_inDisplayFingerprintColor));
 
-        setImageResource(R.drawable.fingerprint_in_screen_default);
+        setImageResource(mIsEnrolling
+            ? R.drawable.fingerprint_in_screen_enrolling
+            : R.drawable.fingerprint_in_screen_default);
 
         mPaintShow.setAntiAlias(true);
         mPaintShow.setColor(res.getColor(R.color.config_inDisplayFingerprintColor));
@@ -255,7 +259,9 @@ public class InScreenFingerprint extends ImageView implements OnTouchListener {
         if (event.getAction() == MotionEvent.ACTION_UP) {
             newInside = false;
             setDim(false);
-            setImageResource(R.drawable.fingerprint_in_screen_default);
+            setImageResource(mIsEnrolling
+                ? R.drawable.fingerprint_in_screen_enrolling
+                : R.drawable.fingerprint_in_screen_default);
         }
 
         if (newInside == mInsideCircle) {
@@ -267,7 +273,9 @@ public class InScreenFingerprint extends ImageView implements OnTouchListener {
         invalidate();
 
         if (!mInsideCircle) {
-            setImageResource(R.drawable.fingerprint_in_screen_default);
+            setImageResource(mIsEnrolling
+                ? R.drawable.fingerprint_in_screen_enrolling
+                : R.drawable.fingerprint_in_screen_default);
             return false;
         }
 
@@ -322,7 +330,9 @@ public class InScreenFingerprint extends ImageView implements OnTouchListener {
                 WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
         mParams.gravity = Gravity.TOP | Gravity.LEFT;
 
-        setImageResource(R.drawable.fingerprint_in_screen_default);
+        setImageResource(isEnrolling
+            ? R.drawable.fingerprint_in_screen_enrolling
+            : R.drawable.fingerprint_in_screen_default);
 
         mWindowManager.addView(this, mParams);
         viewAdded = true;
